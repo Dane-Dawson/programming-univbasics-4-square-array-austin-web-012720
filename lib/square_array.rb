5a935b083f9cbb550121060bc5d77708f7a9da24
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-13 18:36:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   counter = 0
   new_array = []
 while numbers[counter] do
-  new_array.push (#{numbers} ** 2)
+  new_array.push (numbers[index] ** 2)
   counter +=1
 end
 end
